7bb79061a55b658eaaf63f6ebdf5cc3ded55b1e5
JTG94 <sky_engel@naver.com>
2020-04-02 13:50:47 +0900

    #4 테스트 코드 검증 서드파티 라이브러리 should 적용

--- utils.spec.js
+++ utils.spec.js
@@ -1,11 +1,12 @@
 const utils = require('./utils');
-const assert = require('assert');
+const should = require('should');
 
 //describe를 활용하여 테스트수트 작성(환경)
 describe('utils.js모듈의 capitalize() 함수는', () => {
     //TestCase 작성
     it('문자열의 첫번째 문자를 대문자로 변환한다', () => {
         const result = utils.capitalize('hello');
-        assert.equal(result, 'Hello');
+        //should 검증 사용시 가독성 향상
+        result.should.be.equal('Hello');
     })
 })
