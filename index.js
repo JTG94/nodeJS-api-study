76324ba22ec14d598848fd22db45509ea00f1111
JTG94 <sky_engel@naver.com>
2020-04-02 13:11:50 +0900

    #1 Test user 조회 Api 작성 - 디비 연동 x

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var users = [
+    {id: 1, name: 'alice'},
+    {id: 2, name: 'bek'},
+    {id: 3, name: 'chris'}
+];
+
+//server console log middleware
+app.use(morgan('dev'));
+
+app.get('/users', function(req, res) {
+    res.json(users);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
