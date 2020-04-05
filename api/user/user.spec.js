4fd602a5356a500d3d6d96ae5ee2720dba8d953f
JTG94 <sky_engel@naver.com>
2020-04-05 15:11:57 +0900

    #12 Node ORM 시퀄라이즈 적용 연습

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -3,9 +3,17 @@
 const request = require('supertest');
 const should = require('should');
 const app = require('../../');
+const models = require('../../models');
 
 //test 수트
 describe('GET /users는 ', () => {
+    const users = [{ name: "alice" }, { name: "bek" }, { name: "chris" }];
+    before(() => {
+        return models.sequelize.sync({ force: true });
+    });
+    before(() => {
+        return models.User.bulkCreate(users);
+    });
     describe('성공시', () => {
         it('유저 객체를 담은 배열로 응답한다', (done) => {  //작성한 api가 비동기로 동작하기때문에 done 콜백 함수 필요
             request(app)
@@ -45,6 +53,13 @@ describe('GET /users는 ', () => {
 
 //user 단건 조회 테스트
 describe('GET /users/1는', () => {
+    const users = [{ name: "alice" }, { name: "bek" }, { name: "chris" }];
+    before(() => {
+        return models.sequelize.sync({ force: true });
+    });
+    before(() => {
+        return models.User.bulkCreate(users);
+    });
     describe('성공시', () => {
         it('id가 1인 유저 객체를 반환한다', (done) => {
             request(app)
@@ -72,105 +87,106 @@ describe('GET /users/1는', () => {
 });
 
 //user 단건 삭제 테스트
-describe('DELETE /users/1', () => {
-    describe('성공시', () => {
-        it('204를 응답한다', (done) => {
-            request(app)
-                .delete('/users/1')
-                .expect(204)
-                .end(done);
-        });
+describe("DELETE /users/1", () => {
+    const users = [{ name: "alice" }, { name: "bek" }, { name: "chris" }];
+    before(() => {
+        return models.sequelize.sync({ force: true });
     });
-    describe('실패시', () => {
-        it('id가 숫자가 아닐경우 400으로 응답한다', done => {
-            request(app)
-                .delete('/users/one')
-                .expect(400)
-                .end(done);
-        });
+    before(() => {
+        return models.User.bulkCreate(users);
     });
+  describe("성공시", () => {
+    it("204를 응답한다", (done) => {
+      request(app).delete("/users/1").expect(204).end(done);
+    });
+  });
+  describe("실패시", () => {
+    it("id가 숫자가 아닐경우 400으로 응답한다", (done) => {
+      request(app).delete("/users/one").expect(400).end(done);
+    });
+  });
 });
 
 //user 단건 등록 테스트 
-describe('POST /users', () => {
-    describe('성공시', () => {
-        let name = 'daniel', body;
-        before(done => {
-            request(app)
-                .post('/users')
-                .send({ name }) //body값 입력
-                .expect(201)
-                .end((err, res) => {
-                    body = res.body;
-                    done();
-                });
-        })
-        it('생성된 유저 객체를 반환한다', () => {
-            body.should.have.property('id');
-        });
-        it('입력한 name을 반환한다', () => {
-            body.should.have.property('name', name);
+describe("POST /users", () => {
+    const users = [{ name: "alice" }, { name: "bek" }, { name: "chris" }];
+    before(() => {
+        return models.sequelize.sync({ force: true });
+    });
+    before(() => {
+        return models.User.bulkCreate(users);
+    });
+  describe("성공시", () => {
+    let name = "daniel",
+      body;
+    before((done) => {
+      request(app)
+        .post("/users")
+        .send({ name }) //body값 입력
+        .expect(201)
+        .end((err, res) => {
+          body = res.body;
+          done();
         });
     });
-    describe('실패시', () => {
-        it('name 파라미터 누락시 400을 반환한다', done => {
-            request(app)
-                .post('/users')
-                .send({})
-                .expect(400)
-                .end(done)
-        });
-        it('name 중복인 경우 409을 반환한다', done => {
-            request(app)
-                .post('/users')
-                .send({ name: 'daniel' })
-                .expect(409)
-                .end(done)
-        });
+    it("생성된 유저 객체를 반환한다", () => {
+      body.should.have.property("id");
     });
+    it("입력한 name을 반환한다", () => {
+      body.should.have.property("name", name);
+    });
+  });
+  describe("실패시", () => {
+    it("name 파라미터 누락시 400을 반환한다", (done) => {
+      request(app).post("/users").send({}).expect(400).end(done);
+    });
+    it("name 중복인 경우 409을 반환한다", (done) => {
+      request(app)
+        .post("/users")
+        .send({ name: "daniel" })
+        .expect(409)
+        .end(done);
+    });
+  });
 });
 
 //user 단건 수정 테스트
-describe('PUT /users/:id', () => {
-    describe('성공시', () => {
-        it('변경된 name을 응답한다', done => {
-            const name = 'chally';
-            request(app)
-                .put('/users/3')
-                .send({ name })
-                .end((err, res) => {
-                    res.body.should.have.property('name', name);
-                    done();
-                });
+describe("PUT /users/:id", () => {
+  const users = [{ name: "alice" }, { name: "bek" }, { name: "chris" }];
+  before(() => {
+    return models.sequelize.sync({ force: true });
+  });
+  before(() => {
+    return models.User.bulkCreate(users);
+  });
+  describe("성공시", () => {
+    it("변경된 name을 응답한다", (done) => {
+      const name = "chally";
+      request(app)
+        .put("/users/3")
+        .send({ name })
+        .end((err, res) => {
+          res.body.should.have.property("name", name);
+          done();
         });
     });
-    describe('성공시', () => {
-        it('정수가 아닌 id일 경우 400을 응답한다', done => {
-            request(app)
-                .put('/users/one')
-                .expect(400)
-                .end(done);
-        });
-        it('name이 없을 경우 400을 응답한다', done => {
-            request(app)
-                .put('/users/1')
-                .send({})
-                .expect(400)
-                .end(done);
-        });
-        it('없는 유저일 경우 404을 응답한다', done => {
-            request(app)
-                .put('/users/999')
-                .send({ name: 'foo' })
-                .expect(404)
-                .end(done);
-        });
-        it('이름이 중복일 경우 409을 응답한다', done => {
-            request(app)
-                .put('/users/3')
-                .send({ name: 'bek' })
-                .expect(409)
-                .end(done);
-        });
+  });
+  describe("성공시", () => {
+    it("정수가 아닌 id일 경우 400을 응답한다", (done) => {
+      request(app).put("/users/one").expect(400).end(done);
     });
+    it("name이 없을 경우 400을 응답한다", (done) => {
+      request(app).put("/users/1").send({}).expect(400).end(done);
+    });
+    it("없는 유저일 경우 404을 응답한다", (done) => {
+      request(app)
+        .put("/users/999")
+        .send({ name: "foo" })
+        .expect(404)
+        .end(done);
+    });
+    it("이름이 중복일 경우 409을 응답한다", (done) => {
+      request(app).put("/users/3").send({ name: "bek" }).expect(409).end(done);
+    });
+  });
 });
